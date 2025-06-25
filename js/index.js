baf4d9c6eda014a895cee265e291714f84fc3266
sesughter01 <charles.avul@gmail.com>
2025-06-25 19:06:29 +0100

    finishing touches

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // MAIN SCRIPTS
+
+const navToggle = document.querySelector('.nav-toggle');
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click',()=>{
+    
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link =>{
+    link.addEventListener('click',()=>{
+        document.body.classList.remove('nav-open');
+    });
+});
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  // Show/hide the button based on scroll position
+  window.onscroll = function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.classList.add('show');
+    } else {
+      scrollToTopBtn.classList.remove('show');
+    }
+  };
